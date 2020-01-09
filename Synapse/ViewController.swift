b702e583c9ff8074a2af52fbf43c5a5feb8834c3
Justin <justinkgibbons@gmail.com>
2020-01-08 16:51:26 -0800

    Initial Commit

---
+++ Synapse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Synapse
+//
+//  Created by Justin Gibbons on 1/8/20.
+//  Copyright © 2020 Justin K Gibbons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

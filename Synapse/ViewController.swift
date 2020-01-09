7570633f0662ed1a0c3dd6fbe09f110491659a60
Justin <justinkgibbons@gmail.com>
2020-01-08 21:42:58 -0800

    add Networking and FeedReader classes

--- Synapse/ViewController.swift
+++ Synapse/ViewController.swift
@@ -8,38 +8,45 @@ class FeedVC: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		print("Feed vc loaded.")
-		download() { xml in
-			let title = xml["rss"]["channel"]["title"].element?.text
-			print(title as Any)
+		Networking().download("https://slatestarcodex.com/feed/") { data in
+			let channel = FeedReader().channel(from: data)
+			print("Channel: \(channel)")
 		}
 	}
 	
-	func download(_ completion: @escaping (XMLIndexer) -> Void) {
-		print("Trying to download...")
-		let url = URL(string: "https://slatestarcodex.com/feed/")!
+	
+}
+
+class Networking {
+	
+	func download(_ path: String, _ completion: @escaping (Data) -> Void) {
+		guard let url = URL(string: path) else {
+			print("Couldn't construct URL from path: \(path)"); return
+		}
 		let session = URLSession.shared
 		let task = session.dataTask(with: url) { (data, response, error) in
 			if let error = error { print("Error: \(error)") }
-			
-			if let data = data {
-				let string = String(data: data, encoding: .utf8)!
-				print("\(string) download complete")
-				let xml = SWXMLHash.parse(data)
-				completion(xml)
-			}
+			if let data = data { completion(data) }
 		}
-		
 		task.resume()
 	}
+	
 }
 
 class FeedReader {
 	
-	func read(from data: Data) {
+	func channel(from data: Data) -> Channel {
 		let xml = SWXMLHash.parse(data)
-		let root = xml["rss"]
+		let channelElement = xml["rss"]["channel"]
 
+		let channel = Channel(context: CoreData.shared.persistentContainer.viewContext)
+		channel.title = channelElement["title"].element?.text
+		channel.link = channelElement["link"].element?.text
+		channel.desc = channelElement["description"].element?.text
+		channel.url = channelElement["atom:link"].element?.attribute(by: "href")?.text
+		channel.language = channelElement["language"].element?.text
 		
+		return channel
 	}
 }
 

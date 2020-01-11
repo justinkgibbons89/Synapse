a4c1c886529e073d860ee758fe10f55d1f9e764a
Justin <justinkgibbons@gmail.com>
2020-01-10 21:23:20 -0800

    add feed functionality

--- Synapse/AddFeedVC.swift
+++ Synapse/AddFeedVC.swift
@@ -1,9 +1,29 @@
 import UIKit
 
-class AddFeedVC: UITableViewController {
+class AddFeedVC: UITableViewController, UISearchBarDelegate {
+	
+	//MARK: IBOutlet
+	@IBOutlet weak var searchBar: UISearchBar!
 	
 	//MARK: UIViewController
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		searchBar.delegate = self
+		searchBar.autocapitalizationType = .none
+	}
+	
+	//MARK: UISearchBar Delegate
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		if let query = searchBar.text?.lowercased() {
+			FeedReader.subscribe(to: query) { channel in
+				print("Subscribed to channel: \(channel)")
+			}
+		}
+		searchBar.resignFirstResponder()
+	}
+	
+	override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		print("Scrolled!")
+		searchBar.resignFirstResponder()
 	}
 }

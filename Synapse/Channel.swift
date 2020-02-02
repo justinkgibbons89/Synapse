d353a0e7ffb7dfa8621f5ad2068d8bda45d412cd
Justin <justinkgibbons@gmail.com>
2020-02-02 01:12:09 -0800

    note xml property

--- Synapse/Channel.swift
+++ Synapse/Channel.swift
@@ -2,6 +2,7 @@ import Foundation
 
 extension Channel {
 	
+	/// The data of the most recently published item attached to the channel. From the RSS `pubDate`property.
 	var mostRecentDate: Date? {
 		let itemSet = self.items as! Set<Item>
 		let sorted = itemSet.sorted { (a, b) -> Bool in

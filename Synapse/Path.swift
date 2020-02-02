be83db299d607fd2b5bfb5bad4d16fe637980d80
Justin <justinkgibbons@gmail.com>
2020-02-02 01:11:33 -0800

    fix typo

--- Synapse/Path.swift
+++ Synapse/Path.swift
@@ -1,7 +1,3 @@
-//
-//  Path.swift
-//  RSS13
-
 import Foundation
 
 class Path {
@@ -10,7 +6,7 @@ class Path {
     static let insecurePrefix = "http://"
     static let securePrefix = "https://"
     
-    /// Returns a `lowercased` path string with an `https://` prefix
+    /// Returns a lowercased  path string with an 'https://' prefix
     /// - Parameter source: The input string to be normalized
     ///
     /// If the string already includes an insecure prefix it will be replaced.
